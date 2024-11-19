f6b480c578cd3fffe4fdbf840fd8eb1d611cdcff
Clement Cabus <chummycabus@gmail.com>
2024-11-19 23:50:34 +0800

    ProfileActivity now connected to the database (still has errors) - connected to the database but still cannot show the details - can now run app on mobile devices (just change the base URL in the RetrofitClient.java class) - added UserDetailsResponse.java

--- app/src/main/java/com/example/barangayinformationsystem/ApiService.java
+++ app/src/main/java/com/example/barangayinformationsystem/ApiService.java
@@ -3,7 +3,9 @@ package com.example.barangayinformationsystem;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public interface ApiService {
     @FormUrlEncoded
@@ -12,4 +14,8 @@ public interface ApiService {
             @Field("username") String username,
             @Field("password") String password
     );
+    @FormUrlEncoded
+    @GET("fetch_user_details.php")
+    Call<UserDetailsResponse> getUserDetails(@Query("id") int userId);
+
 }
